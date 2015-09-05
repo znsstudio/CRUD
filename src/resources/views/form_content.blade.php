10ac17500ee6d1aec8b68e673e3283e8a7ee55dc
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-09-05 18:31:28 +0300

    added select_from_array field type and fixed new fields overwriting mechanism

--- src/resources/views/form_content.blade.php
+++ src/resources/views/form_content.blade.php
@@ -14,7 +14,7 @@
   {{-- Show the inputs --}}
   @foreach ($crud['fields'] as $field)
     <!-- load the view from the application if it exists, otherwise load the one in the package -->
-	@if(view()->exists('vendor.dick.crud.form_content'))
+	@if(view()->exists('vendor.dick.crud.fields.'.$field['type']))
 		@include('vendor.dick.crud.fields.'.$field['type'], array('field' => $field))
 	@else
 		@include('crud::fields.'.$field['type'], array('field' => $field))
