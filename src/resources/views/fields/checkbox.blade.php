22f1be45d101c6243e04dabe811777489d85b16c
Cristian Tabacitu <cristitabacitu@gmail.com>
2015-09-07 23:08:52 +0300

    added checkbox field

--- src/resources/views/fields/checkbox.blade.php
+++ src/resources/views/fields/checkbox.blade.php
@@ -1,1 +1,16 @@
-[//TODO: checkbox]
+<!-- checkbox field -->
+<div class="checkbox">
+	<label>
+	  <input type="hidden" name="{{ $field['name'] }}" value="0">
+	  <input type="checkbox" @foreach ($field as $attribute => $value)
+    		@if( $attribute == 'value' )
+    			@if((int) $value == 1)
+    			checked = "checked"
+    			@endif
+    			{{ $attribute }}= "1"
+    		@else
+    			{{ $attribute }}="{{ $value }}"
+    		@endif
+    	@endforeach> {{ $field['label'] }}
+	</label>
+</div>
