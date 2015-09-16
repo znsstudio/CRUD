0d279fb20bb1c3a8ee3bcfc69a1adf565f1cfec9
Alin Ghitu <alin@updivision.com>
2015-09-16 15:49:43 +0300

    Changed the name for AdminLTE folder to start with uppercase

--- src/resources/views/fields/assets/js/ckeditor.blade.php
+++ src/resources/views/fields/assets/js/ckeditor.blade.php
@@ -1,6 +1,6 @@
 <!-- include summernote js-->
-<script src="{{ asset('adminLTE/plugins/ckeditor/ckeditor.js') }}"></script>
-<script src="{{ asset('adminLTE/plugins/ckeditor/adapters/jquery.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/ckeditor/ckeditor.js') }}"></script>
+<script src="{{ asset('AdminLTE/plugins/ckeditor/adapters/jquery.js') }}"></script>
 <script>
     jQuery(document).ready(function($) {
     	$('textarea.ckeditor' ).ckeditor({
