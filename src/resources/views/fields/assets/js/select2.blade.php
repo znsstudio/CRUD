e789f877fc67b13ed241a986fba0e17077e7f0a2
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-02-23 12:56:33 +0200

    fixes dick issue #55 - multiple select fields in the same form issue

--- src/resources/views/fields/assets/js/select2.blade.php
+++ src/resources/views/fields/assets/js/select2.blade.php
@@ -2,6 +2,13 @@
 <script src="{{ asset('dick/js/vendor/select2/select2.js') }}"></script>
 <script>
 	jQuery(document).ready(function($) {
-		$('.select2').select2();
+		// trigger select2 for each untriggered select box
+		$('.select2').each(function (i, obj) {
+            if ($(obj).attr("data-select2") != 'true')
+            {
+                $(obj).select2();
+                $(obj).attr('select2', 'true');
+            }
+        });
 	});
 </script>
