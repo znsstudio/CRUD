f6eddd0f6152e484467271a10068460f881ed54c
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-07-22 19:47:32 +0300

    fixed error showing

--- src/resources/views/form_content.blade.php
+++ src/resources/views/form_content.blade.php
@@ -1,14 +1,16 @@
 <form role="form">
   {{-- Show the erros, if any --}}
   @if ($errors->any())
-  	<div class="callout callout-danger">
-        <h4>{{ trans('backpack::crud.please_fix') }}</h4>
-        <ul>
-		@foreach($errors->all() as $error)
-			<li>{{ $error }}</li>
-		@endforeach
-		</ul>
-	</div>
+  	<div class="col-md-12">
+  		<div class="callout callout-danger">
+	        <h4>{{ trans('backpack::crud.please_fix') }}</h4>
+	        <ul>
+			@foreach($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+			</ul>
+		</div>
+  	</div>
   @endif
 
   {{-- Show the inputs --}}
