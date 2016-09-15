67fea585d339496b6c57444d1c15a222c5b69dbb
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-09-15 06:10:58 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/CrudTrait.php
+++ src/CrudTrait.php
@@ -110,25 +110,22 @@ trait CrudTrait
         // if a new file is uploaded, delete the file from the disk
         if ($request->hasFile($attribute_name) &&
             $this->{$attribute_name} &&
-            $this->{$attribute_name} != null)
-        {
+            $this->{$attribute_name} != null) {
             \Storage::disk($disk)->delete($this->{$attribute_name});
             $this->attributes[$attribute_name] = null;
         }
 
         // if the file input is empty, delete the file from the disk
-        if (is_null($value) && $this->{$attribute_name} != null)
-        {
+        if (is_null($value) && $this->{$attribute_name} != null) {
             \Storage::disk($disk)->delete($this->{$attribute_name});
             $this->attributes[$attribute_name] = null;
         }
 
         // if a new file is uploaded, store it on disk and its filename in the database
-        if ($request->hasFile($attribute_name) && $request->file($attribute_name)->isValid())
-        {
+        if ($request->hasFile($attribute_name) && $request->file($attribute_name)->isValid()) {
             // 1. Generate a new file name
             $file = $request->file($attribute_name);
-            $new_file_name = md5($file->getClientOriginalName().time()).".".$file->getClientOriginalExtension();
+            $new_file_name = md5($file->getClientOriginalName().time()).'.'.$file->getClientOriginalExtension();
             // 2. Move the new file to the correct path
             $file_path = $file->storeAs($destination_path, $new_file_name, $disk);
 
