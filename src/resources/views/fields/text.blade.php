ed966ec6c4ef332c066250b75d1d51af2001c7fe
Cristian Tabacitu <cristitabacitu@gmail.com>
2016-09-16 09:33:01 +0300

    allow prefix and suffix to include html

--- src/resources/views/fields/text.blade.php
+++ src/resources/views/fields/text.blade.php
@@ -3,14 +3,14 @@
     <label>{!! $field['label'] !!}</label>
 
     @if(isset($field['prefix']) || isset($field['suffix'])) <div class="input-group"> @endif
-        @if(isset($field['prefix'])) <div class="input-group-addon">{{ $field['prefix'] }}</div> @endif
+        @if(isset($field['prefix'])) <div class="input-group-addon">{!! $field['prefix'] !!}</div> @endif
         <input
             type="text"
             name="{{ $field['name'] }}"
             value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
             @include('crud::inc.field_attributes')
         >
-        @if(isset($field['suffix'])) <div class="input-group-addon">{{ $field['suffix'] }}</div> @endif
+        @if(isset($field['suffix'])) <div class="input-group-addon">{!! $field['suffix'] !!}</div> @endif
     @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
 
     {{-- HINT --}}
