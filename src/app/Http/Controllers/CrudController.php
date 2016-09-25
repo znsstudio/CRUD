3433abda39997306ce0300b4463a29f88eff58c5
OwenMelbz <owenmelbz@gmail.com>
2016-09-25 17:34:31 +0100

    removing relational fields from ajax table query
    DataTable cant accept the fake/relational columns, so they need
    stripping from the collection first.

--- src/app/Http/Controllers/CrudController.php
+++ src/app/Http/Controllers/CrudController.php
@@ -324,8 +324,11 @@ class CrudController extends BaseController
 
         // add the primary key, even though you don't show it,
         // otherwise the buttons won't work
-        $columns = collect($this->crud->columns)->pluck('name')->merge($this->crud->model->getKeyName())->toArray();
-
+        //we also need to remove relational fields from the columns
+        $columns = collect($this->crud->columns)->filter(function($column, $key){
+            return !isset($column['entity']) && !isset($column['model']); //we assume this is a relational field
+        })->pluck('name')->merge($this->crud->model->getKeyName())->toArray();
+        
         // structure the response in a DataTable-friendly way
         $dataTable = new DataTable($this->crud->query, $columns);
 
