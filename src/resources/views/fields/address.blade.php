864312849e923587b8911c9ce25bc8d50fa17041
Owen Melbourne <owen@selesti.com>
2016-09-27 14:07:31 +0100

    added address_full flag to save json rsponse
    usage
    
    ```php
    $this->crud->addField([
                'name' => 'address',
                'type' => 'address',
                'address_full' => true, //need to cast the field as array
                'label' => 'Address Lookup'
            ]);
    ```
    
    using `address_full` will save the almost-full response back from
    Algolia
    
    Additionally you need to cast your field as an array/object

--- src/resources/views/fields/address.blade.php
+++ src/resources/views/fields/address.blade.php
@@ -4,13 +4,22 @@
 
     @if(isset($field['prefix']) || isset($field['suffix'])) <div class="input-group"> @endif
         @if(isset($field['prefix'])) <div class="input-group-addon">{!! $field['prefix'] !!}</div> @endif
+        @if(isset($field['address_full']) && $field['address_full'])
         <input
             type="text"
-            data-address="{{ $field['name'] }}"
+            data-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['address_full']) && $field['address_full'] ? 'true' : 'false'}} }"
+            @include('crud::inc.field_attributes')
+        >
+        <input type="hidden" value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}" name="{{ $field['name'] }}">
+        @else
+        <input
+            type="text"
+            data-address="{&quot;field&quot;: &quot;{{$field['name']}}&quot;, &quot;full&quot;: {{isset($field['address_full']) && $field['address_full'] ? 'true' : 'false'}} }"
             name="{{ $field['name'] }}"
             value="{{ old($field['name']) ? old($field['name']) : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}"
             @include('crud::inc.field_attributes')
         >
+        @endif
         @if(isset($field['suffix'])) <div class="input-group-addon">{!! $field['suffix'] !!}</div> @endif
     @if(isset($field['prefix']) || isset($field['suffix'])) </div> @endif
 
@@ -45,9 +54,28 @@
             window.AlgoliaPlaces = window.AlgoliaPlaces || {};
 
             $('[data-address]').each(function(){
-                window.AlgoliaPlaces[ $(this).data('address') ] = places({
-                    container: $(this)[0]
+
+                var $this      = $(this),
+                $addressConfig = $this.data('address'),
+                $field = $('[name="'+$addressConfig.field+'"]'),
+                $place = places({
+                    container: $this[0]
                 });
+
+                if( $addressConfig.full ){
+
+                    $place.on('change', function(e){
+                        var result = JSON.parse(JSON.stringify(e.suggestion));
+                        delete(result.highlight); delete(result.hit); delete(result.hitIndex);
+                        delete(result.rawAnswer); delete(result.query);
+                        $field.val( JSON.stringify(result) );
+                    });
+
+                    var existingData = JSON.parse($field.val());
+                    $this.val(existingData.value);
+                }
+
+                window.AlgoliaPlaces[ $addressConfig.field ] = $place;
             });
         });
     </script>
