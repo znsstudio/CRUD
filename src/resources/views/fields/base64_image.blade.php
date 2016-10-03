e5b9ec5c63ac6a107d946841286df1fd2628b360
OwenMelbz <owenmelbz@gmail.com>
2016-10-03 12:34:06 +0100

    base64_image default and hint added

--- src/resources/views/fields/base64_image.blade.php
+++ src/resources/views/fields/base64_image.blade.php
@@ -1,11 +1,11 @@
-  <div class="form-group col-md-12 image" data-preview="#{{ $field['name'] }}" data-aspectRatio="{{ $field['aspect_ratio'] }}" data-crop="{{ $field['crop'] }}">
+  <div class="form-group col-md-12 image" data-preview="#{{ $field['name'] }}" data-aspectRatio="{{ isset($field['aspect_ratio']) ? $field['aspect_ratio'] : 0 }}" data-crop="{{ isset($field['crop']) ? $field['crop'] : false }}" @include('crud::inc.field_wrapper_attributes')>
     <div>
         <label>{!! $field['label'] !!}</label>
     </div>
     <!-- Wrap the image or canvas element with a block element (container) -->
     <div class="row">
         <div class="col-sm-6" style="margin-bottom: 20px;">
-            <img id="mainImage" src="{{ isset($field['src']) ? $entry->where('id', $entry->id)->first()->{$field['src']}() : (isset($field['value']))?$field['value']:'' }}">
+            <img id="mainImage" src="{{ isset($field['src']) ? $entry->where('id', $entry->id)->first()->{$field['src']}() : (isset($field['value']) ? $field['value'] : (isset($field['default']) ? $field['default'] : '' )) }}">
         </div>
         @if($field['crop'])
         <div class="col-sm-3">
@@ -21,7 +21,7 @@
     <div class="btn-group">
         <label class="btn btn-primary btn-file">
             Choose file <input type="file" accept="image/*" id="uploadImage" class="hide">
-            <input type="hidden" id="hiddenImage" name="{{ $field['name'] }}">
+            <input type="hidden" id="hiddenImage" name="{{ $field['name'] }}" @include('crud::inc.field_attributes')>
         </label>
         @if($field['crop'])
         <button class="btn btn-default" id="rotateLeft" type="button" style="display: none;"><i class="fa fa-rotate-left"></i></button>
@@ -32,6 +32,11 @@
         @endif
         <button class="btn btn-danger" id="remove" type="button"><i class="fa fa-trash"></i></button>
     </div>
+
+    {{-- HINT --}}
+    @if (isset($field['hint']))
+        <p class="help-block">{!! $field['hint'] !!}</p>
+    @endif
   </div>
 
 
