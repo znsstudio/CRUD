edc80d06e569e67648ba93d216e0ea62dd3d66f2
OwenMelbz <owenmelbz@gmail.com>
2016-10-04 13:19:26 +0100

    enforced formatting

--- src/resources/views/fields/date_picker.blade.php
+++ src/resources/views/fields/date_picker.blade.php
@@ -13,6 +13,7 @@
     <label>{!! $field['label'] !!}</label>
     <div class="input-group date">
         <input
+            readonly="readonly"
             data-bs-datepicker="{{ isset($field['date_picker_options']) ? json_encode($field['date_picker_options']) : '{}'}}"
             type="text"
             @include('crud::inc.field_attributes')
@@ -36,6 +37,11 @@
     {{-- FIELD CSS - will be loaded in the after_styles section --}}
     @push('crud_fields_styles')
     <link rel="stylesheet" href="/vendor/adminlte/plugins/datepicker/datepicker3.css">
+    <style media="screen">
+        .input-group.date input {
+            background-color: #fff;
+        }
+    </style>
     @endpush
 
     {{-- FIELD JS - will be loaded in the after_scripts section --}}
@@ -57,7 +63,7 @@
                 }, $fake.data('bs-datepicker'));
                 $picker = $fake.datepicker($customConfig);
 
-                preparedDate = new Date($field.val()).format($fake.data('bg-datepicker-format'));
+                preparedDate = new Date($field.val()).format($customConfig.format);
                 $fake.val(preparedDate);
                 $picker.datepicker('update', preparedDate);
 
